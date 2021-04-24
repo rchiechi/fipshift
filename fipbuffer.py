5fd18fce059b6017b72d6cdaf42ee8d8255d2ac5
Ryan Chiechi <r.c.chiechi@rug.nl>
2021-04-24 16:43:05 +0200

    Initial commit

---
+++ fipbuffer.py
@@ -0,0 +1,82 @@
+'''Open FIP MP3 url and buffer it to disk for time-shifted re-streaming.'''
+
+import os
+import urllib.request
+import threading
+import queue
+import time
+
+
+FIPURL='http://icecast.radiofrance.fr/fip-midfi.mp3'
+BLOCKSIZE=1024*128
+# pylint: disable=missing-class-docstring, missing-function-docstring
+
+class FIPBuffer(threading.Thread):
+
+    def __init__(self, _alive, _fqueue, _tmpdir):
+        threading.Thread.__init__(self)
+        self.setName('File Buffer Thread')
+        self.alive = _alive
+        self.fqueue = _fqueue
+        self.tmpdir = _tmpdir
+        self.f_counter=0
+        self.t_start = time.time()
+
+    def run(self):
+        print("Starting %s" % self.getName())
+        req = urllib.request.urlopen(FIPURL, timeout=10)
+        while True:
+            buff = req.read(BLOCKSIZE)
+            if not buff:
+                print("%s: emtpy block, dying." % self.getName())
+                self.alive.clear()
+                break
+            print("%s: fetching block" % self.getName())
+            fn = os.path.join(self.tmpdir, self.getfn())
+            with open(fn, 'wb') as fh:
+                fh.write(buff)
+                self.fqueue.put( (time.time(), fn) )
+                print("%s: wrote %s" % (self.getName(), fn))
+            self.f_counter += 1
+            if not self.alive.is_set():
+                print("%s: dying." % self.getName())
+                break
+
+    def getfn(self):
+        return str(self.f_counter).zfill(16)
+    def getruntime(self):
+        return time.time() - self.t_start
+
+
+
+if __name__ == "__main__":
+    TMPDIR='/tmp/fipshift'
+    buffertime = 60
+    if not os.path.exists(TMPDIR):
+        os.mkdir(TMPDIR)
+    for tmpfn in os.listdir(TMPDIR):
+        print(os.path.join(TMPDIR,tmpfn))
+        os.remove(os.path.join(TMPDIR,tmpfn))
+    alive = threading.Event()
+    fqueue = queue.Queue()
+    alive.set()
+    fipbuffer = FIPBuffer(alive, fqueue, TMPDIR)
+    fipbuffer.start()
+    time.sleep(1)
+    while True:
+        try:
+            print("Thread running for %0.1f seconds" % fipbuffer.getruntime())
+            print("Wrote %s files (%s kb)" % (
+                int(fipbuffer.getfn()), int(fipbuffer.getfn())*BLOCKSIZE) )
+            if time.time() - fipbuffer.getruntime() > buffertime:
+                _f = fqueue.get()
+                while time.time() - _f[0] < buffertime:
+                    time.sleep(0.1)
+                print("Deleting %s" % _f[1])
+                os.remove(os.path.join(TMPDIR, _f[1]))
+            time.sleep(3)
+        except KeyboardInterrupt:
+            print("Killing %s" % fipbuffer.getName())
+            alive.clear()
+            fipbuffer.join()
+            break
---
+++ shout-fip.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+# usage: ./example.py /path/to/file1 /path/to/file2 ...
+import os
+import sys
+import threading
+import queue
+import time
+import shout
+from fipbuffer import FIPBuffer
+
+TMPDIR='/tmp/fipshift'
+buffertime = 60
+
+t_start = time.time()
+
+if not os.path.exists(TMPDIR):
+    os.mkdir(TMPDIR)
+for tmpfn in os.listdir(TMPDIR):
+    print("Clearning %s" % os.path.join(TMPDIR,tmpfn))
+    os.remove(os.path.join(TMPDIR,tmpfn))
+
+alive = threading.Event()
+fqueue = queue.Queue()
+alive.set()
+fipbuffer = FIPBuffer(alive, fqueue, TMPDIR)
+fipbuffer.start()
+
+try:
+    while time.time() - t_start < buffertime:
+        print("Buffering for %0.0f more seconds." % (buffertime - (time.time() - t_start)) )
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Killing %s" % fipbuffer.getName())
+    alive.clear()
+    fipbuffer.join()
+    sys.exit()
+
+s = shout.Shout()
+print("Using libshout version %s" % shout.version())
+
+s.host = '10.9.8.4'
+# s.port = 8000
+# s.user = 'source'
+s.password = 'im08en'
+s.mount = "/fip.mp3"
+s.format = 'mp3'
+# s.format = 'vorbis' | 'mp3'
+# s.protocol = 'http' | 'xaudiocast' | 'icy'
+s.name = 'Time-shifted FIP Radio'
+# s.genre = ''
+# s.url = ''
+s.public = 0 
+
+# s.audio_info = { 'key': 'val', ... }
+#  (keys are shout.SHOUT_AI_BITRATE, shout.SHOUT_AI_SAMPLERATE,
+#   shout.SHOUT_AI_CHANNELS, shout.SHOUT_AI_QUALITY)
+s.open()
+
+total = 0
+st = time.time()
+
+while True:
+    try:
+        _f = fqueue.get()
+        fa = _f[1]
+        print("opening file %s" % fa)
+        with open(fa, 'rb') as fh:
+            #TODO: can we extract this from metadata?
+            s.set_metadata({'song': fa})
+            nbuf = fh.read(4096)
+            while True:
+                buf = nbuf
+                nbuf = fh.read(4096)
+                total = total + len(buf)
+                if len(buf) == 0:
+                    break
+                s.send(buf)
+                s.sync()
+        print("Deleting %s" % _f[1])
+        os.remove(os.path.join(TMPDIR, _f[1]))
+    except KeyboardInterrupt:
+        print("Killing %s" % fipbuffer.getName())
+        alive.clear()
+        fipbuffer.join()
+        break
+et = time.time()
+br = total*0.008/(et-st)
+print("Sent %d bytes in %d seconds (%f kbps)" % (total, et-st, br))
+s.close()
