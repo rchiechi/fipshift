cd0969a43902f88caae02e7e19dd698f0b9d300d
Ryan Chiechi <r.c.chiechi@rug.nl>
2021-04-25 16:10:35 +0200

    Pretty up the output

---
+++ options.py
@@ -0,0 +1,41 @@
+'''Parse command line args and config files'''
+import os
+import argparse
+import configparser
+import shutil
+
+def parseopts():
+    '''Parse commandline arguments and config file and return opts, config'''
+
+    desc='''Create an shoutcast client that time-shifts
+            FIP for listening in a different timezone.'''
+    parser = argparse.ArgumentParser(description=desc,
+                                  formatter_class=argparse
+                                  .ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-d','--delay', action="store", default=15*3600,
+         type=int,
+         help="Delay time in seconds.")
+
+    parser.add_argument('--configdir', action="store",
+         default=os.path.join(os.path.expanduser('~'),'.config'),
+         help="Set the dir to look for the config file.")
+
+    opts=parser.parse_args()
+    config = doconfig(os.path.join(opts.configdir,
+          'fipshift.conf')
+          )
+    return opts,config
+
+def doconfig(config_file):
+    '''Parse config file or write a default file.'''
+    if not os.path.exists(config_file):
+        _pwd = os.path.dirname(os.path.realpath(__file__))
+        shutil.copy2(os.path.join(_pwd,'template.conf'), config_file)
+        return None
+    config = configparser.ConfigParser(allow_no_value=False)
+    config.read(config_file)
+    if 'USEROPTS' not in config:
+        config = configparser.ConfigParser(allow_no_value=False)
+        config.read(os.path.basename(config_file))
+    return config
--- shout-fip.py
+++ shout-fip.py
@@ -9,25 +9,24 @@ import queue
 import time
 import shout
 from fipbuffer import FIPBuffer
+from options import parseopts
 
 # pylint: disable=missing-class-docstring, missing-function-docstring
 
-
-TMPDIR='/tmp/fipshift'
 ALIVE = threading.Event()
-buffertime = 15*3600 #CET->PST
-
-if len(sys.argv) > 1:
-    buffertime = int(sys.argv[1])
-if buffertime < 60:
-    print("Delay must be more than 60 sec.")
-    sys.exit()
-if len(sys.argv) > 1:
-    TMPDIR = sys.argv[2]
 
 def killbuffer(signum, frame): # pylint: disable=unused-argument
-    print("Received %s, killing buffer thread." % signum)
+    print("\nReceived %s, killing buffer thread." % signum)
     ALIVE.clear()
+
+def cleantmpdir(tmpdir):
+    n = 0
+    for tmpfn in os.listdir(tmpdir):
+        sys.stdout.write("\rClearning %s " % os.path.join(tmpdir,tmpfn))
+        sys.stdout.flush()
+        os.remove(os.path.join(tmpdir,tmpfn))
+        n += 1
+    print("\rCleaned: %s files.                                   " % n)
 
 def timeinhours(sec):
     sec_value = sec % (24 * 3600)
@@ -39,11 +38,14 @@ def timeinhours(sec):
 
 ##### MAIN() ######
 
+opts, config = parseopts()
+
+TMPDIR = os.path.join(config['USEROPTS']['TMPDIR'], 'fipshift')
+
 if not os.path.exists(TMPDIR):
     os.mkdir(TMPDIR)
-for tmpfn in os.listdir(TMPDIR):
-    print("Clearning %s" % os.path.join(TMPDIR,tmpfn))
-    os.remove(os.path.join(TMPDIR,tmpfn))
+print("Saving files to %s" % TMPDIR)
+cleantmpdir(TMPDIR)
 
 signal.signal(signal.SIGHUP, killbuffer)
 fqueue = queue.Queue()
@@ -53,8 +55,8 @@ fipbuffer.start()
 time.sleep(3)
 
 try:
-    while fipbuffer.getruntime() < buffertime:
-        _remains = (buffertime - fipbuffer.getruntime())/60
+    while fipbuffer.getruntime() < opts.delay:
+        _remains = (opts.delay - fipbuffer.getruntime())/60 or 1
         sys.stdout.write("\rBuffering for %0.0f min. " % _remains)
         sys.stdout.flush()
         time.sleep(10)
@@ -67,17 +69,17 @@ except KeyboardInterrupt:
 s = shout.Shout()
 print("Using libshout version %s" % shout.version())
 
-s.host = '10.9.8.13'
-s.port = 8000
-s.user = 'source'
-s.password = 'im08en'
-s.mount = "/fip.mp3"
+s.host = config['USEROPTS']['HOST']
+s.port = int(config['USEROPTS']['PORT'])
+s.user = config['USEROPTS']['USER']
+s.password = config['USEROPTS']['PASSWORD']
+s.mount = config['USEROPTS']['MOUNT']
+s.name = config['USEROPTS']['NAME']
+s.genre = config['USEROPTS']['GENRE']
+s.url = config['USEROPTS']['URL']
+s.public = int(config['USEROPTS']['PUBLIC'])
 s.format = 'mp3'
 s.protocol = 'http'
-s.name = 'Time-shifted FIP Radio'
-s.genre = 'ecletic'
-s.url = 'https://www.fip.fr'
-s.public = 0
 s.audio_info = {shout.SHOUT_AI_SAMPLERATE: '48000',
                 shout.SHOUT_AI_CHANNELS: '2',
                 shout.SHOUT_AI_BITRATE: '128'}
@@ -88,13 +90,17 @@ except shout.ShoutException as msg:
     killbuffer('SHOUTERROR',None)
     sys.exit(1)
 
+sys.stdout.write("\n\n\n")
 while not fqueue.empty():
     try:
         _f = fqueue.get(timeout=10)
         fa = _f[1]
         _h, _m = timeinhours( time.time()-_f[0] )
-        sys.stdout.write("\rOpening %s (%0.0fh:%0.0fm) \"%s\"  " % (
-            fa, _h, _m, _f[2]['track']) )
+        _mb = (fqueue.qsize()*128)/1024
+        sys.stdout.write("\033[F\033[F\rOpening %s (%0.0fh:%0.0fm) %0.2f MB \n" % (
+            fa, _h, _m, _mb ) )
+        sys.stdout.write("\rTrack: %s \n" % _f[2]['track'])
+        sys.stdout.write("\rArtist: %s " % _f[2]['artist'])
         sys.stdout.flush()
         with open(fa, 'rb') as fh:
             s.set_metadata({'song': _f[2]['track'],
@@ -117,3 +123,4 @@ while not fqueue.empty():
 killbuffer('EMPTYQUEUE',None)
 fipbuffer.join()
 s.close()
+cleantmpdir(TMPDIR)
