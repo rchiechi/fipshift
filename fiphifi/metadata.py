19ae2a30b1e6888f3893e7ef3aebe579cb9757da
Papa <rchiechi@familymac.local>
2023-09-20 17:41:02 -0400

    No?

--- fiphifi/metadata.py
+++ fiphifi/metadata.py
@@ -105,8 +105,8 @@ class FIPMetadata(threading.Thread):
         return _metadata
 
     def _getmeta(self, when):
-        _metadata = self.metadata.get(when, METATEMPLATE[when])[0]
-        if _metadata is None or not isinstance(_metadata, dict):
+        _metadata = self.metadata.get(when, METATEMPLATE[when])
+        if _metadata is not isinstance(_metadata, dict):
             logger.warn("%s metadata is: %s", self.name, _metadata)
             _metadata = {}
         try:
