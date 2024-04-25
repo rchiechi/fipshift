1f8b17d10908fd1e002078814828363f147b07c6
Ryan Chiechi <rchiech@ncsu.edu>
2024-04-25 19:04:49 -0400

    Raise OSError if playlist is neglected for > 5 min

--- fiphifi/buffer.py
+++ fiphifi/buffer.py
@@ -181,6 +181,9 @@ class Playlist():
     def _get_playing(self):
         if self.lastupdate > TSLENGTH+1 and self.lastupdate < 1000:
             logger.warning("Playlist updated more than %ss ago (%0.0f)", TSLENGTH, self.lastupdate)
+        if self.lastupdate > 300 < 1000:
+            logger.warning("It's been more than 5 minutes, dying.")
+            raise OSError("Playlist neglected!")
         proc = self._get_ffmpeg_proc()
         if proc is None:
             return -1
